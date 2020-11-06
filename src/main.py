9a654a75fd3944ebe91ff97f8c5a12e95a549ba4
Junha Park <jpark3@scu.edu>
2020-11-05 19:50:30 -0800

    (0.0.2) Send JoJo reference

--- src/main.py
+++ src/main.py
@@ -2,17 +2,35 @@ import discord
 import os
 from dotenv import load_dotenv
 
-def main():
-  client = discord.Client()
+def LOG_C(message):
+  print(f'client ({message.author}) sent: {message.content}')
 
-  @client.event
-  async def on_ready():
-    print(f'KONO {client.user} DA!')
+def LOG_S(client, response):
+  print(f'server ({client.user}) sent: {response}')
 
-  client.run(ENV['TOKEN'])
+class KermitClient(discord.Client):
+  async def on_ready(self):
+    print(f'bot connected: {client.user}')
+    for guild in client.guilds:
+      if guild.name == ENV['GUILD']:
+        print(f'bot found discord guild: {guild.name}')
+
+  async def on_message(self, message):
+    # Ignore messages from the bot itself to avoid getting stuck in a loop
+    if message.author == client.user:
+      return
+
+    if message.content == '$konobotda':
+      LOG_C(message)
+      response = 'It was me, Kermit Bot!'
+      await message.channel.send(response)
+      LOG_S(client, response)
 
 if __name__ == '__main__':
   load_dotenv()
-  ENV = {}
-  ENV['TOKEN'] = os.getenv('DISCORD_TOKEN')
-  main()
+  ENV = {
+    'TOKEN': os.getenv('TOKEN'),
+    'GUILD': os.getenv('GUILD'),
+  }
+  client = KermitClient()
+  client.run(ENV['TOKEN'])
