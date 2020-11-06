af0c911fab05518230941eea29d111f19bc22db6
Junha Park <jpark3@scu.edu>
2020-11-05 23:32:37 -0800

    (0.0.4) More JoJo references

--- src/main.py
+++ src/main.py
@@ -1,30 +1,44 @@
-import discord
-import os
+from discord import Client
 from dotenv import load_dotenv
+from random import randint
+import os
 
-def LOG_C(message):
-  print(f'client ({message.author}) sent: {message.content}')
+class KermitClient(Client):
+  def LogClient(self, message):
+    print(f'client ({message.author}) sent: {message.content}')
 
-def LOG_S(client, response):
-  print(f'server ({client.user}) sent: {response}')
+  def LogServer(self, response):
+    print(f'server ({self.user}) sent: {response}')
 
-class KermitClient(discord.Client):
   async def on_ready(self):
-    print(f'bot connected: {client.user}')
-    for guild in client.guilds:
+    print(f'bot connected: {self.user}')
+    for guild in self.guilds:
       if guild.name == ENV['GUILD']:
         print(f'bot found discord guild: {guild.name}')
 
   async def on_message(self, message):
     # Ignore messages from the bot itself to avoid getting stuck in a loop
-    if message.author == client.user:
+    if message.author == self.user:
       return
 
-    if message.content == '$konobotda':
-      LOG_C(message)
-      response = 'It was me, Kermit Bot!'
+    if message.content == '$jojo':
+      self.LogClient(message)
+
+      responses = [
+        'It was me, Kermit Bot!',
+        'KONO DIO DA',
+        'Goodbye, JoJo!',
+        'ROOO DOOO ROOO RAAA DAAA',
+        'ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA',
+        'WRRRRYYYYY',
+        'Daga kotowaru!',
+        'I can even lift this rock?',
+        'How many slices of bread have you eaten?',
+        'NOROI NOROI ZA WARUDO WA SAIKYO NO STANDO DA'
+      ]
+      response = responses[randint(0, len(responses) - 1)]
       await message.channel.send(response)
-      LOG_S(client, response)
+      self.LogServer(response)
 
 if __name__ == '__main__':
   load_dotenv()
