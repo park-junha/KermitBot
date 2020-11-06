1c8e6e8554e59cb9f409a91850780bebbefd875f
Junha Park <48537888+park-junha@users.noreply.github.com>
2020-11-06 14:16:25 -0800

    (0.1.0) Modularize the bot (#1)
    * Modularize KermitClient
    
    * Ignore swap files
    
    * Modularize jojo command
    
    * gamers command
    
    * Add function runner helper

---
+++ src/app/app.py
@@ -0,0 +1,37 @@
+from discord import Client
+from app.commands import jojo, gamers
+
+class KermitClient(Client):
+  def set_env(self, env):
+    self.local_env = {
+      'guild': env['guild']
+    }
+
+  def LogClient(self, message: str):
+    print(f'client ({message.author}) sent: {message.content}')
+
+  def LogServer(self, response: str):
+    print(f'server ({self.user}) sent: {response}')
+
+  async def run_function(self, function, message):
+    self.LogClient(message)
+    response: str = function()
+    await message.channel.send(response)
+    self.LogServer(response)
+
+  async def on_ready(self):
+    print(f'bot connected: {self.user}')
+    for guild in self.guilds:
+      if guild.name == self.local_env['guild']:
+        print(f'bot found discord guild: {guild.name}')
+
+  async def on_message(self, message: str):
+    # Ignore messages from the bot itself to avoid getting stuck in a loop
+    if message.author == self.user:
+      return
+
+    if message.content == '$jojo':
+      await self.run_function(jojo.respond, message)
+
+    if message.content.lower() == '$gamers':
+      await self.run_function(gamers.respond, message)
--- src/main.py
+++ src/main.py
@@ -1,50 +1,13 @@
-from discord import Client
+from app.app import KermitClient
 from dotenv import load_dotenv
-from random import randint
 import os
-
-class KermitClient(Client):
-  def LogClient(self, message):
-    print(f'client ({message.author}) sent: {message.content}')
-
-  def LogServer(self, response):
-    print(f'server ({self.user}) sent: {response}')
-
-  async def on_ready(self):
-    print(f'bot connected: {self.user}')
-    for guild in self.guilds:
-      if guild.name == ENV['GUILD']:
-        print(f'bot found discord guild: {guild.name}')
-
-  async def on_message(self, message):
-    # Ignore messages from the bot itself to avoid getting stuck in a loop
-    if message.author == self.user:
-      return
-
-    if message.content == '$jojo':
-      self.LogClient(message)
-
-      responses = [
-        'It was me, Kermit Bot!',
-        'KONO DIO DA',
-        'Goodbye, JoJo!',
-        'ROOO DOOO ROOO RAAA DAAA',
-        'ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA ORA',
-        'WRRRRYYYYY',
-        'Daga kotowaru!',
-        'I can even lift this rock?',
-        'How many slices of bread have you eaten?',
-        'NOROI NOROI ZA WARUDO WA SAIKYO NO STANDO DA'
-      ]
-      response = responses[randint(0, len(responses) - 1)]
-      await message.channel.send(response)
-      self.LogServer(response)
 
 if __name__ == '__main__':
   load_dotenv()
-  ENV = {
-    'TOKEN': os.getenv('TOKEN'),
-    'GUILD': os.getenv('GUILD'),
+  env = {
+    'token': os.getenv('TOKEN'),
+    'guild': os.getenv('GUILD')
   }
   client = KermitClient()
-  client.run(ENV['TOKEN'])
+  client.set_env(env)
+  client.run(env['token'])
