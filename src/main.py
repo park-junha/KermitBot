86668da8d79e0ef8157b90fbd0c768cd3bf29c6a
Junha Park <jpark3@scu.edu>
2020-11-05 19:13:37 -0800

    (0.0.1) Add initial dependencies and let the bot speak

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
-print("KONO KERUMITTO BOTTO DA!")
+import discord
+import os
+from dotenv import load_dotenv
+
+def main():
+  client = discord.Client()
+
+  @client.event
+  async def on_ready():
+    print(f'KONO {client.user} DA!')
+
+  client.run(ENV['TOKEN'])
+
+if __name__ == '__main__':
+  load_dotenv()
+  ENV = {}
+  ENV['TOKEN'] = os.getenv('DISCORD_TOKEN')
+  main()
